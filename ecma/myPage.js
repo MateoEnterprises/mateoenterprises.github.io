c607bbfa3a128ed6d1449e1991232965724b3179
MateoEnterprises <mat.thompson02@gmail.com>
2016-08-27 16:46:36 -0600

    SPA action going on. Now the real work begins.

--- ecma/myPage.js
+++ ecma/myPage.js
@@ -1,7 +1,33 @@
-var myPage = angular.module('myPage', []);
+var myPage = angular.module('myPage', ['ngRoute']);
 
-myPage.controller('mainController', ['$scope', '$log', function($scope, $log) {
+myPage.config(function ($routeProvider) {
 
+	$routeProvider
+
+	//**********COVER/LANDING**********
+	.when('/', {
+		templateUrl: 'pages/cover.html',
+		controller: 'coverController'
+	})
+
+	//**********ABOUT ME**********
+	.when('/about', {
+		templateUrl: 'pages/about.html',
+		controller: 'aboutController'
+	})
+
+});
+
+myPage.controller('coverController', ['$scope', '$log', function($scope, $log) {
+
+	//tested to make sure everything is working properly.
 	$scope.greeting = "Hello World!";
+
+}]);
+
+myPage.controller('aboutController', ['$scope', '$log', function($scope, $log) {
+
+	//tested to make sure everything is working properly.
+	$scope.greeting = "About Me";
 
 }]);
