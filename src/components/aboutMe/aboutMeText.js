fc6196daad551bd64c5ef02ec349e673ebd75b26
MateoEnterprises <mat.thompson02@gmail.com>
2017-09-07 10:41:38 -0600

    updating BG and aboutMeText and various styles

--- src/components/aboutMe/aboutMeText.js
+++ src/components/aboutMe/aboutMeText.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 const AboutMeText = () => {
   return (
-    <span>
-      Random text about myself. Random text about myself. Random text about myself. Random text about myself. Random text about myself. Random text about myself. Random text about myself. 
-    </span>
+    <div className="aboutMeText">
+      <p>As someone who enjoys creating and improving things, I found front-end development a few years ago and began the process of self learning while working full time. After a short stint I put myself through a coding boot camp at DevMtn where I still worked full time so I could expedite switching careers.</p>
+      <p>Having been in web-development for 2 years now, I thuroughly enjoy coding and improving websites through UI/UX updates and planning for the future. Being able to create and improve is a passion I have for work and life.</p>
+      <p>Coming from a managerial postion for 3PL where I handled customers such as Boeing and Northrop Grumman I harnessed those skills for effeciency and precision to get tasks done correctly. </p>
+      <p>Reach out to me if you have any questions via <a href="https://www.linkedin.com/in/mateoenterprises">Linkedin</a> or <a href="https://www.facebook.com/mathew.thompson2">Facebook</a></p>
+      <p>I am currently working on teaching myself React<img className="img20px" src="./images/react.svg"/> and Redux<img className="img15px" src="./images/redux.svg"/></p>
+    </div>
   )
 }
 
