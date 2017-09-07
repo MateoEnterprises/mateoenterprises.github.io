5597a0252ef2d3b5934c7ca57291ac9529906e05
MateoEnterprises <mat.thompson02@gmail.com>
2017-09-07 14:54:08 -0600

    finshed up AboutMe and updated CV to link to google instead of a PDF so resume linked is always up to date.

--- src/components/aboutMe/aboutMeText.js
+++ src/components/aboutMe/aboutMeText.js
@@ -7,7 +7,7 @@ const AboutMeText = () => {
       <p>Having been in web-development for 2 years now, I thuroughly enjoy coding and improving websites through UI/UX updates and planning for the future. Being able to create and improve is a passion I have for work and life.</p>
       <p>Coming from a managerial postion for 3PL where I handled customers such as Boeing and Northrop Grumman I harnessed those skills for effeciency and precision to get tasks done correctly. </p>
       <p>Reach out to me if you have any questions via <a href="https://www.linkedin.com/in/mateoenterprises">Linkedin</a> or <a href="https://www.facebook.com/mathew.thompson2">Facebook</a></p>
-      <p>I am currently working on teaching myself React<img className="img20px" src="./images/react.svg"/> and Redux<img className="img15px" src="./images/redux.svg"/></p>
+      <p>I am currently working on teaching myself React<img className="img25px" src="./images/react.png"/> and Redux<img className="img25px" src="./images/redux-white.svg"/></p>
     </div>
   )
 }
--- src/components/titleBar/name.js
+++ src/components/titleBar/name.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Name = () => {
   return (
     <span className="navbar-brand">
-      <a href="./style/cv.pdf">Mathew Thompson</a>
+      <a href="https://docs.google.com/document/d/1RNRBpNhOlHX3pnxWilzNJ5CNLauhIclTuqLzA2P3YAg/edit?usp=sharing" target="_blank">MathewThompson.cv</a>
     </span>
   )
 }
